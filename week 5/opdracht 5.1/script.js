3070216d558c886ad3686c504aa471628d6f9e39
Azfar05 <112875863+Azfar05@users.noreply.github.com>
2022-11-03 14:44:36 +0100

    Add files via upload
    BIEMMM!!!!!

---
+++ week 5/opdracht 5.1/script.js
@@ -0,0 +1,20 @@
+alert("Hallo wereld!");
+
+//kleur aanpassen
+document.getElementById("title").style.color = "red";
+
+//variabelen gebruiken
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 * getal2;
+alert("Het antwoord is: " + som)
+
+// promptbox
+var kleur = prompt("Geef een Engelse kleurnaam: (red of blue", "");
+
+//kleur aanpassen
+document.getElementById("title").style.color = "kleur";
+//document.getElementById("title").style.color = "red";
+
+
+document.getElementById("p1").innerHTML = "Hello "+ kleur +"!";
---
+++ week 6/opdracht 6.2/script.js
@@ -0,0 +1,11 @@
+function kleur_aanpassen(){
+    document.getElementById("kleur").style.color = "blue";
+    document.getElementById("kleur").innerText = "Inhoud aangepast";
+}
+
+kleur_aanpassen();
+
+
+
+
+
